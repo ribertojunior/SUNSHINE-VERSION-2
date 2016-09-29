f103d4df8dcaeb0dd5132ee3c7de1e4747b82501
Riberto Junior <ribertojunior@gmail.com>
2016-09-29 16:46:07 -0300

    adding drawable files

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -17,8 +17,14 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.ShareActionProvider;
@@ -32,7 +38,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.android.sunshine.app.data.WeatherContract;
+
 import java.util.ArrayList;
+
+import static com.example.android.sunshine.app.ForecastFragment.COL_WEATHER_DATE;
+import static com.example.android.sunshine.app.ForecastFragment.COL_WEATHER_DESC;
+import static com.example.android.sunshine.app.ForecastFragment.COL_WEATHER_MAX_TEMP;
+import static com.example.android.sunshine.app.ForecastFragment.COL_WEATHER_MIN_TEMP;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -97,12 +110,13 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class DetailFragment extends Fragment {
+    public static class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
         private final String LOG_TAG = DetailFragment.class.getSimpleName();
         ArrayAdapter<String> mDetailAdapter;
         private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
-        String mForecastStr;
+        Uri mForecastUri;
+        private static final int LOADER_ID = 15;
         public DetailFragment() {
             setHasOptionsMenu(true);
         }
@@ -113,9 +127,11 @@ public class DetailActivity extends ActionBarActivity {
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             Intent intent = getActivity().getIntent();
-            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            if (intent != null) {
+                mForecastUri = Uri.parse(intent.getDataString());
+            }
             ArrayList<String> data = new ArrayList<String>();
-            data.add(mForecastStr);
+            //data.add(mForecastUri);
             mDetailAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, data);
             ListView listView = (ListView) rootView.findViewById(R.id.listview_detail);
             listView.setAdapter(mDetailAdapter);
@@ -143,10 +159,57 @@ public class DetailActivity extends ActionBarActivity {
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
             shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
             shareIntent.setType("text/plain");
-            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + " " + FORECAST_SHARE_HASHTAG);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastUri + " " + FORECAST_SHARE_HASHTAG);
             return shareIntent;
 
         }
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            getLoaderManager().initLoader(LOADER_ID, null, this);
+            super.onActivityCreated(savedInstanceState);
+        }
 
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+            return new CursorLoader(getActivity(), mForecastUri, ForecastFragment.getForecastColumns(), null, null, sortOrder);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            ArrayList<String> strings = new ArrayList<String>();
+            strings.add(convertCursorRowToUXFormat(data));
+            mDetailAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, strings);
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            //mDetailAdapter = null;
+        }
+
+        /**
+         * Prepare the weather high/lows for presentation.
+         */
+        private String formatHighLows(double high, double low) {
+            boolean isMetric = Utility.isMetric(getActivity());
+            String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
+            return highLowStr;
+        }
+
+        /*
+            This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
+            string.
+         */
+        private String convertCursorRowToUXFormat(Cursor cursor) {
+            cursor.moveToNext();
+            String highAndLow = formatHighLows(
+                    cursor.getDouble(COL_WEATHER_MAX_TEMP),
+                    cursor.getDouble(COL_WEATHER_MIN_TEMP));
+
+            return Utility.formatDate(cursor.getLong(COL_WEATHER_DATE)) +
+                    " - " + cursor.getString(COL_WEATHER_DESC) +
+                    " - " + highAndLow;
+        }
     }
 }
